e0dc5161b63fc8cfc1503611c339b6bdad78f27e
Szakács Máté <szakacs.mate@burg.lo.hu>
2025-09-30 08:59:51 +0200

    3-4

---
+++ Barlangok/Barlangok/Pages/Shared/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Barlangok.Data;
+using Barlangok.Models;
+
+namespace Barlangok.Pages.Shared
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Barlangok.Data.BarlangDBContext _context;
+
+        public DeleteModel(Barlangok.Data.BarlangDBContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Barlang Barlang { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barlang = await _context.Barlangok.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (barlang == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Barlang = barlang;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barlang = await _context.Barlangok.FindAsync(id);
+            if (barlang != null)
+            {
+                Barlang = barlang;
+                _context.Barlangok.Remove(Barlang);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
